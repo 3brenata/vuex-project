72b825b59bff23871164145229aefa0268c9d294
Fabricio Aquiles <70663657+fabricioaquiles@users.noreply.github.com>
2025-03-26 07:56:24 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <main>
+    <ShoppingCart />
+  </main>
+</template>
+<script>
+import ShoppingCart from "./components/ShoppingCart.vue";
+
+export default {
+  components: {
+    ShoppingCart,
+  },
+};
+</script>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="cart">
+    <h2>Carrinho de Compras</h2>
+    <button @click="addItemToCart">Adicionar Produto</button>
+    <p>Total de itens no carrinho: {{ cartItemCount }}</p>
+  </div>
+</template>
+
+<style scoped>
+.cart {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+}
+
+.cart button {
+  background: transparent;
+  border: 1px solid greenyellow;
+  padding: 15px;
+  border-radius: 10px;
+  color: white;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.cart button:hover{
+  background: greenyellow;
+  transition: 0.5s;
+}
+.cart .clicked {
+  background: rgba(172, 255, 47, 0.55) !important;
+  transition: 0.5s;
+}
+</style>
+
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['cartItemCount']),
+  },
+  methods: {
+    ...mapActions(['addToCart']),
+    addItemToCart() {
+      const product = { id: Date.now(), name: 'Produto Exemplo' };
+      this.addToCart(product);
+    },
+  },
+};
+</script>
